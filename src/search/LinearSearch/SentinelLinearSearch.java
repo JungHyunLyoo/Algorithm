aac9285ced81b5431161c6c078c67ab9a1338f58
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-03-12 23:30:55 +0900

    sentinelLinearSearch

--- src/search/LinearSearch/SentinelLinearSearch.java
+++ src/search/LinearSearch/SentinelLinearSearch.java
@@ -1,5 +1,40 @@
 package search.LinearSearch;
 
-public class SentinelLinearSearch {
+import java.util.Scanner;
 
+public class SentinelLinearSearch {
+	static private Scanner stdIn=new Scanner(System.in);
+	
+	static int sentinelLinearSearch(int[] x,int size,int key) {
+		x[size]=key;
+		int i=0;
+		while(true) {
+			if(x[i]==key) 
+				break;
+			i++;
+		}
+		return i==size?-1:i;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("array size:");
+		int size=stdIn.nextInt();
+		int[] x=new int[size+1];
+
+		for(int i=0;i<size;i++) {
+			System.out.println("x["+i+"]:");
+			x[i]=stdIn.nextInt();
+		}
+
+		System.out.println("value to search:");
+		int key=stdIn.nextInt();
+		
+		int result=sentinelLinearSearch(x,size,key);
+		if(result==-1) {
+			System.out.println("not found");
+		}
+		else {
+			System.out.println("index:"+result);			
+		}
+	}
 }
