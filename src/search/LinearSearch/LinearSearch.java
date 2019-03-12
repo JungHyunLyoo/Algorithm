145859b74cb578bd59f281b0d81e28d219bda0ec
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-03-12 23:21:37 +0900

    make LinearSearch package and SentinelLinearSearch

--- src/search/LinearSearch/LinearSearch.java
+++ src/search/LinearSearch/LinearSearch.java
@@ -1,4 +1,4 @@
-package search;
+package search.LinearSearch;
 import java.util.Scanner;
 
 public class LinearSearch {
---
+++ src/search/LinearSearch/SentinelLinearSearch.java
@@ -0,0 +1,5 @@
+package search.LinearSearch;
+
+public class SentinelLinearSearch {
+
+}
