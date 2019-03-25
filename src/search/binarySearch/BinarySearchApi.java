6f1d8a754d2d8aa2e3be4cbbd9e3b6bb5c7830cc
Charles_Lyoo <dkcmsadkcmsa@gmail.com>
2019-03-25 22:32:05 +0900

    create BinaraySearchApi.java

---
+++ src/search/binarySearch/BinarySearchApi.java
@@ -0,0 +1,31 @@
+package search.binarySearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearchApi {
+	static Scanner stdIn=new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("array size:");
+		int size=stdIn.nextInt();
+		int[] x=new int[size+1];
+
+		System.out.println("please insert number by asc:");
+		for(int i=0;i<size;i++) {
+			System.out.println("x["+i+"]:");
+			x[i]=stdIn.nextInt();
+		}
+
+		System.out.println("value to search:");
+		int key=stdIn.nextInt();
+		
+		int result=Arrays.binarySearch(x, key);
+		if(result==-1) {
+			System.out.println("not found");
+		}
+		else {
+			System.out.println("index:"+result);			
+		}
+	}
+}
